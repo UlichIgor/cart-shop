fe93d073df8dc12f73fb214c258082be4210f7fa
Ulich Igor <ulichigor55@gmail.com>
2025-02-24 09:21:36 +0200

    configuring webpack to use the html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,48 @@
 const path = require("path");
+
 module.exports = {
   entry: "./src/index.js",
-  output: { path: path.resolve(__dirname, "build") },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
   module: {
     rules: [
       {
-        test: /.(js|jsx)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ["@babel/preset-env", "@babel/preset-react"]
           }
-        },
+        }
       },
-    ],
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    setupMiddlewares: (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+      devServer.app.get('/some/path', function (req, res) {
+        res.json({ custom: 'response' });
+      });
+
+      return middlewares;
+    },
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+
 };
